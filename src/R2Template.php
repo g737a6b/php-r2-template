ce7da87d8e31a0c978423a9de68a8ce5a79561a7
Hiroyuki Suzuki <github@mofg.net>
2024-01-01 18:05:38 +0900

    Do fix

--- src/R2Template.php
+++ src/R2Template.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace R2Template;
 
 /**
@@ -9,117 +10,128 @@ namespace R2Template;
  * @copyright Copyright (c) 2021 Hiroyuki Suzuki mofg.net
  * @license http://opensource.org/licenses/MIT The MIT License
  */
-class R2Template{
-	/**
-	 * @var array
-	 */
-	protected $vars = [];
+class R2Template
+{
+    /**
+     * @var array
+     */
+    protected $vars = [];
 
-	/**
-	 * @var array
-	 */
-	protected $_vars = [];
+    /**
+     * @var array
+     */
+    protected $_vars = [];
 
-	/**
-	 * @var string
-	 */
-	protected $file = "";
+    /**
+     * @var string
+     */
+    protected $file = "";
 
-	/**
-	 * @var string
-	 */
-	protected $path;
+    /**
+     * @var string
+     */
+    protected $path;
 
-	/**
-	 * @param string $path (optional)
-	 */
-	public function __construct(string $path = ""){
-		$this->path = $path;
-	}
+    /**
+     * @param string $path (optional)
+     */
+    public function __construct(string $path = "")
+    {
+        $this->path = $path;
+    }
 
-	/**
-	 * @param string $path
-	 */
-	public function setPath(string $path) : void{
-		$this->path = $path;
-	}
+    /**
+     * @param string $path
+     */
+    public function setPath(string $path): void
+    {
+        $this->path = $path;
+    }
 
-	/**
-	 * @param string $name
-	 * @param mixed $value
-	 */
-	public function set(string $name, $value) : void{
-		$this->vars[$name] = $value;
-	}
+    /**
+     * @param string $name
+     * @param mixed $value
+     */
+    public function set(string $name, $value): void
+    {
+        $this->vars[$name] = $value;
+    }
 
-	/**
-	 * @param string $name
-	 * @return mixed
-	 */
-	public function get(string $name){
-		return ( isset($this->vars[$name]) ) ? $this->vars[$name] : null;
-	}
+    /**
+     * @param string $name
+     * @return mixed
+     */
+    public function get(string $name)
+    {
+        return (isset($this->vars[$name])) ? $this->vars[$name] : null;
+    }
 
-	public function clearVars() : void{
-		$this->vars = [];
-	}
+    public function clearVars(): void
+    {
+        $this->vars = [];
+    }
 
-	/**
-	 * @param string $file
-	 * @param array $vars (optional)
-	 * @param bool $usePath (optional)
-	 * @return bool
-	 */
-	public function display(string $file, array $vars = [], bool $usePath = true) : bool{
-		$this->file = ( $usePath ) ? rtrim($this->path, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR.$file : $file;
-		if( !file_exists($this->file) ){
-			trigger_error("Template ({$this->file}) was not found.", E_USER_WARNING);
-			return false;
-		}else if( !is_readable($this->file) ){
-			trigger_error("Template ({$this->file}) is not readable.", E_USER_WARNING);
-			return false;
-		}
-		$this->_vars = array_merge($this->vars, $vars);
-		unset($file);
-		unset($vars);
-		unset($usePath);
-		extract($this->_vars);
-		$result = include($this->file);
-		return ( $result !== false ) ? true : false;
-	}
+    /**
+     * @param string $file
+     * @param array $vars (optional)
+     * @param bool $usePath (optional)
+     * @return bool
+     */
+    public function display(string $file, array $vars = [], bool $usePath = true): bool
+    {
+        $this->file = ($usePath) ? rtrim($this->path, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR.$file : $file;
+        if(!file_exists($this->file)) {
+            trigger_error("Template ({$this->file}) was not found.", E_USER_WARNING);
+            return false;
+        } elseif(!is_readable($this->file)) {
+            trigger_error("Template ({$this->file}) is not readable.", E_USER_WARNING);
+            return false;
+        }
+        $this->_vars = array_merge($this->vars, $vars);
+        unset($file);
+        unset($vars);
+        unset($usePath);
+        extract($this->_vars);
+        $result = include($this->file);
+        return ($result !== false) ? true : false;
+    }
 
-	/**
-	 * @param string $file
-	 * @param array $vars (optional)
-	 * @param bool $usePath (optional)
-	 * @return string
-	 */
-	public function getContents(string $file, array $vars = [], bool $usePath = true) : string{
-		ob_start();
-		$this->display($file, $vars, $usePath);
-		$result = ob_get_contents();
-		ob_end_clean();
-		return $result;
-	}
+    /**
+     * @param string $file
+     * @param array $vars (optional)
+     * @param bool $usePath (optional)
+     * @return string
+     */
+    public function getContents(string $file, array $vars = [], bool $usePath = true): string
+    {
+        ob_start();
+        $this->display($file, $vars, $usePath);
+        $result = ob_get_contents();
+        ob_end_clean();
+        return $result;
+    }
 
-	/**
-	 * @deprecated
-	 */
-	public function set_dir(string $path) : void{
-		$this->setPath($path);
-	}
+    /**
+     * @deprecated
+     */
+    public function set_dir(string $path): void
+    {
+        $this->setPath($path);
+    }
 
-	/**
-	 * @deprecated
-	 */
-	public function clear_vars() : void{
-		$this->clearVars();
-	}
+    /**
+     * @deprecated
+     */
+    public function clear_vars(): void
+    {
+        $this->clearVars();
+    }
 
-	/**
-	 * @deprecated
-	 */
-	public function get_contents(string $file, array $vars = [], bool $usePath = true) : string{
-		return $this->getContents($file, $vars, $usePath);
-	}
+    /**
+     * @deprecated
+     */
+    public function get_contents(string $file, array $vars = [], bool $usePath = true): string
+    {
+        return $this->getContents($file, $vars, $usePath);
+    }
 }
--- tests/R2TemplateTest.php
+++ tests/R2TemplateTest.php
@@ -1,66 +1,75 @@
 <?php
+
 require(__DIR__."/../autoload.php");
 
 use PHPUnit\Framework\TestCase;
 use R2Template\R2Template;
 
-class R2TemplateTest extends TestCase{
-	public function testSetAndGet(){
-		$Template = new R2Template();
-		$value = "string";
-		$Template->set("var1", $value);
-		$this->assertSame($value, $Template->get("var1"));
-		$this->assertNull($Template->get("var2"));
-	}
+class R2TemplateTest extends TestCase
+{
+    public function testSetAndGet()
+    {
+        $Template = new R2Template();
+        $value = "string";
+        $Template->set("var1", $value);
+        $this->assertSame($value, $Template->get("var1"));
+        $this->assertNull($Template->get("var2"));
+    }
 
-	public function testDisplayWithConstructor(){
-		$path = __DIR__."/templates";
-		$text = "Silence is golden.";
-		$this->expectOutputString($text);
-		$Template = new R2Template($path);
-		$Template->set("var", $text);
-		$Template->display("echo_var.php");
-	}
+    public function testDisplayWithConstructor()
+    {
+        $path = __DIR__."/templates";
+        $text = "Silence is golden.";
+        $this->expectOutputString($text);
+        $Template = new R2Template($path);
+        $Template->set("var", $text);
+        $Template->display("echo_var.php");
+    }
 
-	public function testDisplayWithoutConstructor(){
-		$path = __DIR__."/templates";
-		$text = "Silence is golden.";
-		$this->expectOutputString($text);
-		$Template = new R2Template();
-		$Template->setPath($path);
-		$Template->set("var", $text);
-		$Template->display("echo_var.php");
-	}
+    public function testDisplayWithoutConstructor()
+    {
+        $path = __DIR__."/templates";
+        $text = "Silence is golden.";
+        $this->expectOutputString($text);
+        $Template = new R2Template();
+        $Template->setPath($path);
+        $Template->set("var", $text);
+        $Template->display("echo_var.php");
+    }
 
-	public function testDisplayWithVars(){
-		$path = __DIR__."/templates";
-		$text = "Silence is golden.";
-		$this->expectOutputString($text);
-		$Template = new R2Template($path);
-		$Template->display("echo_var.php", ["var" => $text]);
-	}
+    public function testDisplayWithVars()
+    {
+        $path = __DIR__."/templates";
+        $text = "Silence is golden.";
+        $this->expectOutputString($text);
+        $Template = new R2Template($path);
+        $Template->display("echo_var.php", ["var" => $text]);
+    }
 
-	public function testGetContents(){
-		$path = __DIR__."/templates";
-		$text = "Silence is golden.";
-		$Template = new R2Template($path);
-		$Template->set("var", $text);
-		$this->assertSame($text, $Template->getContents("echo_var.php"));
-	}
+    public function testGetContents()
+    {
+        $path = __DIR__."/templates";
+        $text = "Silence is golden.";
+        $Template = new R2Template($path);
+        $Template->set("var", $text);
+        $this->assertSame($text, $Template->getContents("echo_var.php"));
+    }
 
-	public function testGetContentsWithVars(){
-		$path = __DIR__."/templates";
-		$text = "Silence is golden.";
-		$Template = new R2Template($path);
-		$this->assertSame($text, $Template->getContents("echo_var.php", ["var" => $text]));
-	}
+    public function testGetContentsWithVars()
+    {
+        $path = __DIR__."/templates";
+        $text = "Silence is golden.";
+        $Template = new R2Template($path);
+        $this->assertSame($text, $Template->getContents("echo_var.php", ["var" => $text]));
+    }
 
-	public function testClearVars(){
-		$Template = new R2Template();
-		$value = "string";
-		$Template->set("var", $value);
-		$this->assertSame($value, $Template->get("var"));
-		$Template->clearVars();
-		$this->assertNull($Template->get("var"));
-	}
+    public function testClearVars()
+    {
+        $Template = new R2Template();
+        $value = "string";
+        $Template->set("var", $value);
+        $this->assertSame($value, $Template->get("var"));
+        $Template->clearVars();
+        $this->assertNull($Template->get("var"));
+    }
 }
