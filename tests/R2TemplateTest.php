2ef7309f4ffd8d6697eb968cdae09f941f27d0e2
Hiroyuki Suzuki <github@mofg.net>
2026-01-02 09:00:47 +0900

    Add PHP 8.5 support

--- tests/R2TemplateTest.php
+++ tests/R2TemplateTest.php
@@ -23,7 +23,8 @@ class R2TemplateTest extends TestCase
         $this->expectOutputString($text);
         $Template = new R2Template($path);
         $Template->set("var", $text);
-        $Template->display("echo_var.php");
+        $result = $Template->display("echo_var.php");
+        $this->assertTrue($result);
     }
 
     public function testDisplayWithoutConstructor()
@@ -71,5 +72,77 @@ class R2TemplateTest extends TestCase
         $this->assertSame($value, $Template->get("var"));
         $Template->clearVars();
         $this->assertNull($Template->get("var"));
+    }
+
+    public function testDisplayFileNotFound()
+    {
+        $path = __DIR__."/templates";
+        $Template = new R2Template($path);
+
+        $warningTriggered = false;
+        set_error_handler(function($errno, $errstr) use (&$warningTriggered) {
+            if ($errno === E_USER_WARNING && mb_strpos($errstr, 'Template') !== false) {
+                $warningTriggered = true;
+            }
+            return true;
+        });
+
+        $result = $Template->display("non_existent_file.php");
+
+        restore_error_handler();
+
+        $this->assertTrue($warningTriggered, 'Expected warning was not triggered');
+        $this->assertFalse($result);
+    }
+
+    public function testDisplayWithUsePathFalse()
+    {
+        $absolutePath = __DIR__."/templates/echo_var.php";
+        $text = "Silence is golden.";
+        $this->expectOutputString($text);
+        $Template = new R2Template();
+        $Template->set("var", $text);
+        $Template->display($absolutePath, [], false);
+    }
+
+    public function testGetContentsWithUsePathFalse()
+    {
+        $absolutePath = __DIR__."/templates/echo_var.php";
+        $text = "Silence is golden.";
+        $Template = new R2Template();
+        $Template->set("var", $text);
+        $this->assertSame($text, $Template->getContents($absolutePath, [], false));
+    }
+
+    public function testVariablesMerge()
+    {
+        $path = __DIR__."/templates";
+        $text1 = "First value";
+        $text2 = "Second value";
+        $this->expectOutputString($text2);
+        $Template = new R2Template($path);
+        $Template->set("var", $text1);
+        $Template->display("echo_var.php", ["var" => $text2]);
+    }
+
+    public function testPathWithTrailingSlash()
+    {
+        $path = __DIR__."/templates/";
+        $text = "Silence is golden.";
+        $this->expectOutputString($text);
+        $Template = new R2Template($path);
+        $Template->set("var", $text);
+        $Template->display("echo_var.php");
+    }
+
+    public function testSetPathWithTrailingSlash()
+    {
+        $path = __DIR__."/templates/";
+        $text = "Silence is golden.";
+        $this->expectOutputString($text);
+        $Template = new R2Template();
+        $Template->setPath($path);
+        $Template->set("var", $text);
+        $Template->display("echo_var.php");
     }
 }
